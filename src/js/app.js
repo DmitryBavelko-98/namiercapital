60e5c05c785e07aa0514e0c573c821e2ce6c6247
DmitryBavelko-98 <bavelko90@gmail.com>
2022-07-19 13:41:02 +0300

    Fix bugs

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import isWebp from './modules/functions.js';
 import handleFilters from './modules/filters.js';
 import changeLinks from './modules/links.js';
+// eslint-disable-next-line no-unused-vars
 import * as burger from './modules/burger.js';
 import changeHeaderColor from './modules/header.js';
 
